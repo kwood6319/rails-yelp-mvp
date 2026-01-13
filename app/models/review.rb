9c649247b8a10fe002fe95f36646489984ced695
Katie Wood <kwood6319@hotmail.co.uk>
2026-01-13 20:57:21 +0900

    seeded

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true, in: 0..5}
+  validates :rating, presence: true, numericality: { only_integer: true, in: 0..5 }
+  # validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
+  # validates :rating, presence: true, numericality: true
 end
