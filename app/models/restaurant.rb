4ed53b959c04081c4ebcc06c999c7fdfd76f5d82
Katie Wood <kwood6319@hotmail.co.uk>
2026-01-13 19:44:46 +0900

    Prepare rails app with external specs

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  valdidates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, presence: true, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'], allow_nil: false}
+
+  has_many :reviews, dependent: :destroy
+end
